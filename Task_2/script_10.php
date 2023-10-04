8abbe642c54364dfab8867f00614113fac6e22d3
Ravi Godhani <99579124+ravigodhani@users.noreply.github.com>
2023-10-04 12:24:44 +0530

    Add files via upload

---
+++ Task_2/script_10.php
@@ -0,0 +1,9 @@
+<!-- Write a PHP script to calculate the area of a rectangle given its length and width. -->
+<?php
+$length = 10; 
+$width = 5;   
+
+$area = $length * $width;
+
+echo "The area of the rectangle is: $area ";
+?>
---
+++ Task_2/script_12.php
@@ -0,0 +1,9 @@
+<!-- Write a PHP script to concatenate two strings and display the combined result. -->
+<?php
+$string1 = "Ravi ";
+$string2 = "Godhani";
+
+$combined = $string1 . $string2;
+
+echo $combined;
+?>
